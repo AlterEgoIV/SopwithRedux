e83ecd4e3f34a60ea1e78d5d2221a326b0234a99
Carl <alterego01010@gmail.com>
2017-02-25 15:20:38 +0000

    Initial commit

---
+++ desktop/src/com/sopwithredux/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sopwithredux.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sopwithredux.SopwithRedux;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SopwithRedux(), config);
+	}
+}
